395deca96f2c09f779fc221f305452b272e6efb4
bhavith01 <bhavithbshetty07@gmail.com>
2024-02-15 09:45:53 +0530

    Your commit message

--- src/components/Navigation.jsx
+++ src/components/Navigation.jsx
@@ -1,23 +1,22 @@
-
-import logo from '../assets/logo.png';
+import logo from "../assets/logo.png";
 
 const Navigation = () => {
-    return (
-        <div>
-            <nav>
-                <div className="logo">
-                    <img src={logo} alt="logo" />
-                </div>
-                <ul className="nav-links">
-                    <li href="#">Menus</li>
-                    <li href="#">Locations</li>
-                    <li href="#">About</li>
-                    <li href="#">Contact</li>
-                </ul>
-                <button>Login</button>
-            </nav>
+  return (
+    <div>
+      <nav>
+        <div className="logo">
+          <img src={logo} alt="logo" className="toplogo" />
         </div>
-    );
+        <ul className="nav-links">
+          <li href="#">Menus</li>
+          <li href="#">Locations</li>
+          <li href="#">About</li>
+          <li href="#">Contact</li>
+        </ul>
+        <button>Login</button>
+      </nav>
+    </div>
+  );
 };
 
 export default Navigation;
